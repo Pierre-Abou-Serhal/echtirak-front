2c750558ff103d286ca8327fe4e42d53dacf15d6
Pierre Abou Serhal <pierre.abouserhal.pas@gmail.com>
2025-12-30 01:37:17 +0200

    Added UAT environment for server and build configs

---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    apiUrl: 'https://uat-api.echtirak.com/api',
+    appName: 'Echtirak',
+    localStorage: {
+        sessionKey: 'echtirak:auth-session:v1',
+    }
+};
