c93125e8f0df6b35c6374fb2f3ae93188109a0cd
Pierre Abou Serhal <pierre.abouserhal.pas@gmail.com>
2025-12-30 01:47:25 +0200

    Changed PROD api URL in env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: 'https://dev-api.echtirak.com/api',
+    apiUrl: 'https://api.echtirak.com/api',
     appName: 'Echtirak',
     localStorage: {
         sessionKey: 'echtirak:auth-session:v1',
