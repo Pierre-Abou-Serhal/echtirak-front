6951e0aba72c6759e8f245df1c5f6d44591ab5e3
Pierre Abou Serhal <pierre.abouserhal.pas@gmail.com>
2026-06-07 01:45:29 +0300

    - Fixed apiUrl for dev environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: 'https://uat-api.echtirak.com/api',
+    apiUrl: 'https://dev-api.echtirak.com/api',
     appName: 'Echtirak',
     localStorage: {
         sessionKey: 'echtirak:auth-session:v1',
